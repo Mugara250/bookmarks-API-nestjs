ce5e0ef9413de508de833324b5c1122ed3ed3d28
Mugara250 <mbonyumugara52@gmail.com>
2026-03-15 08:22:42 +0200

    user

---
+++ src/user/guards/roles.guard.ts
@@ -0,0 +1,27 @@
+import {
+  CanActivate,
+  ExecutionContext,
+  ForbiddenException,
+  Injectable,
+} from '@nestjs/common';
+import { Reflector } from '@nestjs/core';
+import { Observable } from 'rxjs';
+
+@Injectable()
+export class RolesGuard implements CanActivate {
+  constructor(private readonly reflector: Reflector) {}
+  public canActivate(
+    context: ExecutionContext,
+  ): boolean | Promise<boolean> | Observable<boolean> {
+    const request = context.switchToHttp().getRequest();
+    const user = request.user;
+    const requiredRoles = this.reflector.getAllAndOverride<string[]>('roles', [
+      context.getHandler(),
+      context.getClass(),
+    ]);
+    if (!requiredRoles) return true;
+    const hasRole = requiredRoles.find((role) => role === user.role);
+    if (!hasRole) throw new ForbiddenException('Your are not an admin');
+    return true;
+  }
+}
--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,13 +1,19 @@
 import {
   Controller,
+  Delete,
   Get,
   HttpCode,
   HttpStatus,
+  Param,
+  ParseIntPipe,
   UseGuards,
 } from '@nestjs/common';
 import { GetUser } from './decorators';
 import { JwtGuard } from './guards';
 import { User } from '@prisma/client';
+import { RolesGuard } from './guards/roles.guard';
+import { Role } from './enums/role.enum';
+import { Roles } from './decorators/roles.decorator';
 
 @UseGuards(JwtGuard)
 @Controller('user')
@@ -22,4 +28,9 @@ export class UserController {
     console.log({ userId, email });
     return user;
   }
+
+  @UseGuards(RolesGuard)
+  @Roles(Role.ADMIN)
+  @Delete(':id')
+  deleteUser(@Param('id', ParseIntPipe) id: number) {}
 }
